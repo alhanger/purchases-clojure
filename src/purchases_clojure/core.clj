c963ea5e34619dccede8ecf339a9bab3adfb5389
alhanger <hanger129@gmail.com>
2015-11-24 14:45:01 -0500

    Parse purchases csv, filter by category according to user input, and write results to a file

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -1,7 +1,27 @@
 (ns purchases-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (let [purchases (slurp "purchases.csv")
+        purchases (str/split-lines purchases)
+        purchases (map (fn [line]
+                         (str/split line #","))
+                       purchases)
+        header (first purchases)
+        purchases (rest purchases)
+        purchases (map (fn [line]
+                         (interleave header line))
+                       purchases)
+        purchases (map (fn [line]
+                         (apply hash-map line))
+                       purchases)
+        purchases (walk/keywordize-keys purchases)
+        _ (println "Search for purchases by category")
+        input (read-line)
+        purchases (filter (fn [line]
+                            (= input (:category line)))
+                          purchases)]
+    (spit "filtered-purchases.edn"
+          (pr-str purchases))))
